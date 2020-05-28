4969f31407af0e9c7283c9613d620f27af066661
radupasparuga <radupasparuga25@gmail.com>
2020-05-28 18:34:31 +0300

    Addded vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <form>
-      <input type="text"><br>
-      <input type="text"><br>
-        <router-link to="/dashboard"><button>submit</button></router-link>
-      <br>
-      <button>clear</button><br>
-    </form>
+    <v-col cols="12" sm="6" md="3">
+      <form>
+        <v-text-field v-model="name" :counter="10" label="Name" required></v-text-field>
+        <v-text-field v-model="email" label="E-mail" required></v-text-field>
+        <v-checkbox v-model="checkbox" label="Do you agree?" required></v-checkbox>
+
+        <v-btn class="mr-4" @click="submit">submit</v-btn>
+        <v-btn @click="clear">clear</v-btn>
+      </form>
+    </v-col>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Login',
-  components: {
-  }
-}
+  name: "Login",
+  components: {}
+};
 </script>
 
 <style>
