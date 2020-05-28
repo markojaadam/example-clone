c7a5ffd29381ce4e1e3133994e512d47f8b810fd
radupasparuga <radupasparuga25@gmail.com>
2020-05-28 18:38:16 +0300

    added redirect to dashboard

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,12 +2,14 @@
   <div>
     <v-col cols="12" sm="6" md="3">
       <form>
-        <v-text-field v-model="name" :counter="10" label="Name" required></v-text-field>
-        <v-text-field v-model="email" label="E-mail" required></v-text-field>
-        <v-checkbox v-model="checkbox" label="Do you agree?" required></v-checkbox>
+        <v-text-field :counter="10" label="Name" required></v-text-field>
+        <v-text-field label="E-mail" required></v-text-field>
+        <v-checkbox label="Do you agree?" required></v-checkbox>
 
-        <v-btn class="mr-4" @click="submit">submit</v-btn>
-        <v-btn @click="clear">clear</v-btn>
+        <router-link to="/dashboard">
+          <v-btn class="mr-4">submit</v-btn>
+        </router-link>
+        <v-btn>clear</v-btn>
       </form>
     </v-col>
   </div>
